7850d2f3635cfc0ef4297936ecdb825ef338729c
weeryan17 <theweeryan17@gmail.com>
2019-09-29 11:58:38 -0500

    Start implementing using main traincarts website for accounts
    This will use oath2 for interactions

--- routes/account.js
+++ routes/account.js
@@ -1,12 +1,7 @@
 var passport = require("passport");
-var localStrategy = require("passport-local");
-var bcrypt = require('bcrypt');
-var discordStrategy = require('passport-discord').Strategy;
+var oauth2 = require("passport-oauth2");
 var express = require('express');
 var router = express.Router();
-passport.use(new localStrategy(function (username, password, done) {
-    done(null, username);
+passport.use(new oauth2({}, function (accessToken, refreshToken, profile, cb) {
 }));
-passport.use(new discordStrategy(config.discord), function (accessToken, refreshToken, profile, cb) {
-});
 //# sourceMappingURL=account.js.map
--- routes/account.ts
+++ routes/account.ts
@@ -1,17 +1,13 @@
 var passport = require("passport");
-var localStrategy = require("passport-local");
-var bcrypt = require('bcrypt');
-var discordStrategy = require('passport-discord').Strategy;
+var oauth2 = require("passport-oauth2");
 
 // @ts-ignore
 var express = require('express');
 var router = express.Router();
 
-passport.use(new localStrategy(function (username : string, password : string, done: (error: any, result: boolean | any, data?: object) => void) {
-    //TODO handle users from database
-    done(null, username)
-}));
+passport.use(new oauth2({
 
-passport.use(new discordStrategy(config.discord), function (accessToken : string, refreshToken : string, profile : object, cb : any) {
-    
-});
+    },
+    function (accessToken : string, refreshToken : string, profile : object, cb: any) {
+
+    }));
